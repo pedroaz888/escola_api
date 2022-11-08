80170469b5cab4e85dfb5fb439145479b29741bb
PedroAz <pdegutty@gmail.com>
2022-11-08 10:48:38 -0300

    gitignore

--- escola_api/settings.py
+++ escola_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kr5+cml(ls+9uckx+q8!koil#2+m(^z6!gl6-4d-$v^#n11xv1'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
